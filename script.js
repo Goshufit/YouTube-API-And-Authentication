cbd22b365c58effb142e30145dce6a7e0d578058
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-06-28 22:17:09 -0400

    Add API variables in JS

---
+++ script.js
@@ -0,0 +1,7 @@
+// Options
+const CLIENT_ID = '418459077117-pchij1fu3469ktv49k3an6du5oc8vlko.apps.googleusercontent.com';
+const DISCOVERY_DOCS = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+const authorizeButton = document.getElementById('authorize-button');
+const signoutButton = document.getElementById('signout-button');
